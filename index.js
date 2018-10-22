a484e27872ab73a81f5bc998a2227603bb986787
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-22 20:24:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
+
+function nestedTarget(){
+  const lis = document
+  .getElementById('nested')
+  .querySelector('div.target');
+  return lis
+}
+
+function increaseRankBy(n) {
+  
+}
